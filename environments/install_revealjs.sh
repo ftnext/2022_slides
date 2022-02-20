7b4ba33db618455184a50d51a2328550a2036f87
ftnext <takuyafjp+develop@gmail.com>
2022-02-20 23:08:02 +0900

    [feat] Install Reveal.js assets

---
+++ environments/install_revealjs.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+SCRIPT_DIR=$(cd "$(dirname "$0")"; pwd)
+PROJECT_ROOT_DIR=$(dirname "${SCRIPT_DIR}")
+
+SITE_PACKAGES_DIR=$(python -c 'import site; print(site.getsitepackages()[0])')
+if [ -e "${SITE_PACKAGES_DIR}/sphinx_revealjs/themes/sphinx_revealjs/static/revealjs4" ]; then
+  echo 'Reveal.js assets are already installed.'
+  exit 0
+fi
+
+mkdir -p "${PROJECT_ROOT_DIR}/tools"
+wget -O "${PROJECT_ROOT_DIR}/tools/fetch_revealjs.py" https://raw.githubusercontent.com/attakei/sphinx-revealjs/master/tools/fetch_revealjs.py
+wget -O "${PROJECT_ROOT_DIR}/package-lock.json" https://raw.githubusercontent.com/attakei/sphinx-revealjs/master/package-lock.json
+
+python "${PROJECT_ROOT_DIR}/tools/fetch_revealjs.py"
+# fetch_revealjs.py creates ${PROJECT_ROOT_DIR}/sphinx_revealjs/themes/sphinx_revealjs/static/revealjs4/{dist,plugin,LICENSE}
+
+mv "${PROJECT_ROOT_DIR}/sphinx_revealjs/themes/sphinx_revealjs/static/revealjs4" "${SITE_PACKAGES_DIR}/sphinx_revealjs/themes/sphinx_revealjs/static/"
+
+rm -r "${PROJECT_ROOT_DIR}/${var:?}" "${PROJECT_ROOT_DIR}/"{package-lock.json,tools,sphinx_revealjs}
