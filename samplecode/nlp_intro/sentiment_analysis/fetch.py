276cd959ae28f0d24577d7b0fb61b3955efcf7df
ftnext <takuyafjp+develop@gmail.com>
2022-06-05 23:05:27 +0900

    [feat] Enable to fetch web resources

--- samplecode/nlp_intro/sentiment_analysis/fetch.py
+++ samplecode/nlp_intro/sentiment_analysis/fetch.py
@@ -1,11 +1,21 @@
-"""青空文庫の『吾輩は猫である』を取得する
+"""Web上のリソースを取得する
 
 ref: 『Pythonによるあたらしいデータ分析の教科書』5章 5.2.5
+
+以下の取得に使用
+- 青空文庫 『吾輩は猫である』
+- 日本語極性評価辞書
 """
 
+import argparse
 import urllib.request
+from urllib.parse import urlparse
 
-urllib.request.urlretrieve(
-    "https://www.aozora.gr.jp/cards/000148/files/789_ruby_5639.zip",
-    "789_ruby_5639.zip",
-)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("url")
+    args = parser.parse_args()
+
+    parsed = urlparse(args.url)
+    output_file = parsed.path.split("/")[-1]
+    urllib.request.urlretrieve(args.url, output_file)
